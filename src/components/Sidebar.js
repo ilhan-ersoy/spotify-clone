33887afed48cc55134468ee314c3ba8cf639ac23
ilhan-ersoy <ilhan.ersoy.ie@gmail.com>
2022-02-26 03:36:50 +0300

    Sidebar 2

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,38 @@
 import spotifyLogo from "../Assets/img/logo.svg";
 import Menu from "./Sidebar/Menu";
+import {LikedSongsIcon, PlusIcon} from "../Icons";
 
 
 export default function Sidebar() {
     return (
         <div className={"w-60 py-6 bg-black flex flex-col "}>
             <img src={spotifyLogo} alt="page_logo" className="h-10 mb-5 pr-12"/>
+
+
             <Menu/>
+
+            <nav className={'mt-6'}>
+                <ul>
+                    <li>
+                        <a href="#" className={'py-2 px-6 flex text-sm text-link group font-semibold hover:text-white items-center'}>
+                            <span className={'w-6 h-6 flex items-center group-hover:bg-opacity-100 bg-opacity-60 justify-center mr-4 bg-white text-sm text-black'}>
+                                <PlusIcon size={'12'}/>
+                            </span>
+                            Çalma Listesi Oluştur
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={'py-2 px-6 group flex text-sm group text-link font-semibold hover:text-white items-center'}>
+                            <span className={'w-6 h-6 flex opacity-60 group-hover:opacity-100 items-center justify-center mr-4 text-sm bg-gradient-to-br from-blue-600 to-indigo-300'}>
+                                <LikedSongsIcon size={'12'}/>
+                            </span>
+                            Beğenilen Şarkılar
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+
         </div>
     )
 }
--- src/components/Sidebar/Menu.js
+++ src/components/Sidebar/Menu.js
@@ -1,30 +1,51 @@
 import {HomeIcon, LibraryIcon, SearchIcon} from "../../Icons";
 import {NavLink} from "react-router-dom";
+import {useState} from "react";
+
 
 export default function Menu() {
+
+
+    const [home, setHome] = useState(false);
+    const [search, setSearch] = useState(false);
+    const [library, setlibrary] = useState(false);
+
+
     return (
         <nav>
             <ul className={"flex flex-col"}>
-                <li>
-                    <NavLink to="/" exact='/' className={"px-6 h-10 gap-x-4 rounded mb-1 flex text-sm text-link hover:text-white items-center"} activeClassName={"bg-active text-white"}>
+                <li onClick={()=>{
+                    setHome(true);
+                    setSearch(false);
+                    setlibrary(false);
+                }}>
+                    <NavLink to="/" exact='/' className={"px-6 h-10 gap-x-4 rounded mb-1 flex text-sm text-link hover:text-white items-center"} activeClassName={" text-white"}>
                         <span>
-                            <HomeIcon />
+                            <HomeIcon selected={home}/>
                         </span>
                         Ana sayfa
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/search' className={"px-6 h-10 gap-x-4 rounded mb-1 flex text-sm font-semibold text-link hover:text-white items-center"} activeClassName={"bg-active text-white"}>
+                <li onClick={()=>{
+                    setHome(false);
+                    setSearch(true);
+                    setlibrary(false);
+                }}>
+                    <NavLink to='/search' className={"px-6 h-10 gap-x-4 rounded mb-1 flex text-sm font-semibold text-link hover:text-white items-center"} activeClassName={" text-white"}>
                         <span>
-                            <SearchIcon/>
+                            <SearchIcon selected={search}/>
                         </span>
                         Ara
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to="/collection" className={"px-6 h-10 gap-x-4 rounded mb-1 flex text-sm font-semibold text-link hover:text-white items-center"} activeClassName={"bg-active text-white"}>
+                <li onClick={()=>{
+                    setHome(false);
+                    setSearch(false);
+                    setlibrary(true);
+                }}>
+                    <NavLink to="/collection" className={"px-6 h-10 gap-x-4 rounded mb-1 flex text-sm font-semibold text-link hover:text-white items-center"} activeClassName={" text-white"}>
                         <span>
-                            <LibraryIcon/>
+                            <LibraryIcon selected={library}/>
                         </span>
                         Kitaplığın
                     </NavLink>
